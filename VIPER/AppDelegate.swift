414754c84d11e5217a67d9a6eaee1eda59e73d04
mhlee <myeonghyeon.lee@gmail.com>
2020-12-04 13:15:29 +0900

    First commit

--- VIPER/AppDelegate.swift
+++ VIPER/AppDelegate.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import SwiftyBeaver
+
+let log = SwiftyBeaver.self
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    log.addDestination(ConsoleDestination())
+    log.info("")
+    
     return true
   }
 
